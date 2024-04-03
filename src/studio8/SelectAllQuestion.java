5898866f75fe84db8cea490cf921e872d69701dc
max <max@10.232.167.95>
2024-04-03 16:57:25 -0500

    did by max

--- src/studio8/SelectAllQuestion.java
+++ src/studio8/SelectAllQuestion.java
@@ -3,13 +3,17 @@ package studio8;
 public class SelectAllQuestion extends MultipleChoiceQuestion {
 
 	public SelectAllQuestion(String prompt, String answer, String[] choices) {
+		super(prompt, answer, choices.length, choices);
+
 		//Hint: 1 point per choice
 		//FIXME
 	}
 	
 	public int checkAnswer(String givenAnswer) {
 		//FIXME Should return partial credit (if earned)!
-		return 0;
+		int Missing = findMissingCorrectAnswers (givenAnswer);
+		int Incorrect = findIncorrectGivenAnswers (givenAnswer);
+		return Missing + Incorrect;
 	}
 
 	private int findMissingCorrectAnswers(String givenAnswer) {
